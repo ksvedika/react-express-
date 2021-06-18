5330c8cd4776238298142addaee72888a89027f7
ksvedika <vedikasankpal16@gmail.com>
2021-06-18 23:44:39 +0530

    react

--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, useState} from 'react';
 import './customers.css';
 import axios from 'axios'
 
 class Customers extends Component {
   constructor() {
     super();
-    this.state = {
-      customers: [],
-      msg:""
-    };
+    const [msg] = useState(0);
+
   }
 
   async componentWillMount() {
     var res = await axios.get("/api/test");
     console.log(res);
-    this.state.msg = res.data.msg
+    this.msg = res.data.msg
+    
   }
 
   render() {
     return (
       <div>
-        <h2>{this.state.msg}</h2>
+        <h2>{this.msg}</h2>
         
       </div>
     );
